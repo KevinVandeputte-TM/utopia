7cfe379af1a54f083abd2beddd88b38f18e5c223
Michal Davidse <r0809700@student.thomasmore.be>
2022-12-04 19:32:46 +0100

    setup network + moving metro

---
+++ Assets/Scripts/metronetwork/MetroController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Sci Fi Train by FiveBrosStopMosYT licensed under CC-BY 4.0.
+
+public class MetroController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+ void Update()
+   {
+      float horizontal = Input.GetAxis("Horizontal");
+      float vertical = Input.GetAxis("Vertical");
+
+
+      Vector2 position = transform.position;
+      position.x = position.x + 15f * horizontal * Time.deltaTime;
+      position.y = position.y + 15f * vertical * Time.deltaTime;
+
+      transform.position = position;
+   }
+}
