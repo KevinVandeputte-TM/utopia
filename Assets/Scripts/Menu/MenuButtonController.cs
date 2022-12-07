ab2af572cbff6fbbcd09197abb311858971d78d8
Kevin Vandeputte <r0792871@student.thomasmore.be>
2022-12-07 19:54:14 +0100

    main menu basis

---
+++ Assets/Scripts/Menu/MenuButtonController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	public int index;
+	[SerializeField] bool keyDown;
+	[SerializeField] int maxIndex;
+	public AudioSource audioSource;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// get the verical input
+		if(Input.GetAxis ("Vertical") != 0){
+			// only trigger on keydown, not on key down hold
+			if(!keyDown){
+				// update the index
+				if (Input.GetAxis ("Vertical") < 0) {
+					if(index < maxIndex){
+						index++;
+					}else{
+						index = 0;
+					}
+				} else if(Input.GetAxis ("Vertical") > 0){
+					if(index > 0){
+						index --; 
+					}else{
+						index = maxIndex;
+					}
+				}
+				keyDown = true;
+			}
+		// set keydown to false for next triggering
+		}else{
+			keyDown = false;
+		}
+	}
+
+}
