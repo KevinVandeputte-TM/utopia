103278d0448bbdee0c0a2e9021ea317c4911a51e
ValerieBecquart <r0809699@student.thomasmore.be>
2023-01-02 20:55:00 +0100

    added script

---
+++ Assets/Scripts/Metrostations/Character_base.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Character_base : MonoBehaviour
+{
+
+    public int questionID_list;
+    public int questionID_api;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Metrostations/Start_World.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+public class Start_World : MonoBehaviour
+{
+
+
+    API_calls sn;
+    public GameObject characterOriginal;
+    public SpriteLibraryAsset color1;
+    public SpriteLibraryAsset color2;
+    public SpriteLibraryAsset color3;
+    public SpriteLibraryAsset color4;
+    public SpriteLibraryAsset color5;
+
+    // Start is called before the first frame update
+    async void Start()
+    {
+
+        //get API SCRIPT OBJECT
+        sn = gameObject.GetComponent<API_calls>();
+
+        //api call to get questions
+        var questionList = await sn.GetQuestionsByStation(9);
+        Debug.Log(questionList);
+
+        //array of colors for character
+        SpriteLibraryAsset[] listOfColors= { color1, color2, color3, color4, color5 };
+
+        int i = 0;
+     
+
+        foreach (var question in questionList)
+        {
+            var position = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+
+            //clone object
+            GameObject otherCharcClone = Instantiate(characterOriginal,position,characterOriginal.transform.rotation);
+
+            //set colorscheme character
+            otherCharcClone.gameObject.GetComponent<SpriteLibrary>().spriteLibraryAsset = listOfColors[i];
+
+            //set variables for questions
+            otherCharcClone.gameObject.GetComponent<Character_base>().questionID_list= i;
+            otherCharcClone.gameObject.GetComponent<Character_base>().questionID_api = question.questionID;
+
+        
+
+            i++;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
