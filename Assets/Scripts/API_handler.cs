1c3074f7bdccbacac412acfce0a227ba9e1c01d3
ValerieBecquart <r0809699@student.thomasmore.be>
2023-01-04 17:18:32 +0100

    removed unused errors

--- Assets/Scripts/API_handler.cs
+++ Assets/Scripts/API_handler.cs
@@ -1,10 +1,6 @@
 using Newtonsoft.Json;
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.Networking;
 
